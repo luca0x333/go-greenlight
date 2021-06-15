7b17d477c2d0ac0c4cd1e32c24744371a3796e9d
luca <luca@nunzi.dev>
2021-06-15 22:42:55 +0100

    add UnmarshalJSON method

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -15,10 +15,10 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 	// When decoding a JSON object into a struct the key/value pairs in the JSON are mapped to
 	// the struct fields json tags..
 	var input struct {
-		Title   string   `json:"title"`
-		Year    int32    `json:"year"`
-		Runtime int32    `json:"runtime"`
-		Genres  []string `json:"genres"`
+		Title   string       `json:"title"`
+		Year    int32        `json:"year"`
+		Runtime data.Runtime `json:"runtime"`
+		Genres  []string     `json:"genres"`
 	}
 
 	// Initialize a new json Decoder instance which reads from the request body and then use
--- internal/data/runtime.go
+++ internal/data/runtime.go
@@ -1,9 +1,15 @@
 package data
 
 import (
+	"errors"
 	"fmt"
 	"strconv"
+	"strings"
 )
+
+// ErrInvalidRuntimeFormat is a custom error returned by UnmarshalJSON() method.
+// It is returned when the method is unable to parse or convert the JSON string successfully.
+var ErrInvalidRuntimeFormat = errors.New("invalid runtime format")
 
 // Runtime type with an underlying type int32.
 type Runtime int32
@@ -19,4 +25,38 @@ func (r Runtime) MarshalJSON() ([]byte, error) {
 
 	// Convert the quoted string to []byte and return it.
 	return []byte(quotedJSONValue), nil
+}
+
+// UnmarshalJSON method attacched to the Runtime custom type so that it satisfies the
+// json.Unmarshaler interface.
+// This method must modify the receiver so we must use a pointer receiver. Otherwise we will only
+// be modifying a copy which is discarded when this method returns.
+func (r *Runtime) UnmarshalJSON(jsonValue []byte) error {
+	// We expect the incoming JSON value will be a string in the format "runtime mins" and the first
+	// thing we need to do is remove the double quotes from this string.
+	unquotedJSONValue, err := strconv.Unquote(string(jsonValue))
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+
+	// Split the string to get the number.
+	parts := strings.Split(unquotedJSONValue, " ")
+
+	// Sanity check the parts of the string to make sure it's in the correct format.
+	if len(parts) != 2 || parts[1] != "mins" {
+		return ErrInvalidRuntimeFormat
+	}
+
+	// Parse the string containing the number into a int32
+	i, err := strconv.ParseInt(parts[0], 10, 32)
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+
+	// Convert the int32 to a Runtime type and assign this to the receiver.
+	// Note that we use use the * operator to deference the receiver (which is a pointer to a Runtime
+	// type) in order to set the underlying value of the pointer.
+	*r = Runtime(i)
+
+	return nil
 }

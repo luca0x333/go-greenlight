1b64a141489ce4a997262febaeb8e1973f4b5e66
luca <luca@nunzi.dev>
2021-07-08 22:28:24 +0100

    add UserModel

--- internal/data/models.go
+++ internal/data/models.go
@@ -13,10 +13,12 @@ var (
 // Models struct wraps the MovieModel.
 type Models struct {
 	Movies MovieModel
+	Users  UserModel
 }
 
 func NewModels(db *sql.DB) Models {
 	return Models{
 		Movies: MovieModel{DB: db},
+		Users:  UserModel{DB: db},
 	}
 }

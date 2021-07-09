b51594c4367f5708ab67afb6cb46633382179e4c
luca <luca@nunzi.dev>
2021-07-09 22:35:45 +0100

    add registerUserHandler

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -22,6 +22,8 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodPatch, "/v1/movies/:id", app.updateMovieHandler)
 	router.HandlerFunc(http.MethodDelete, "/v1/movies/:id", app.deleteMovieHandler)
 
+	router.HandlerFunc(http.MethodPost, "/v1/users", app.registerUserHandler)
+
 	// recoverPanic > rateLimit > router
 	return app.recoverPanic(app.rateLimit(router))
 }
--- internal/data/movies.go
+++ internal/data/movies.go
@@ -24,25 +24,6 @@ type Movie struct {
 	Version   int32     `json:"version"`           // The version number starts at 1 and will be incremented each
 }
 
-func ValidateMovie(v *validator.Validator, movie *Movie) {
-	// Use the Check() method to execute our validation checks.
-	// It will add the key and error message to the errors map if the checks are not true.
-	v.Check(movie.Title != "", "title", "must be provided")
-	v.Check(len(movie.Title) <= 500, "title", "must not be more than 500 bytes long")
-
-	v.Check(movie.Year != 0, "year", "must be provided")
-	v.Check(movie.Year >= 1888, "year", "must be greater than 1888")
-	v.Check(movie.Year <= int32(time.Now().Year()), "year", "must not be in the future")
-
-	v.Check(movie.Runtime != 0, "runtime", "must be provided")
-	v.Check(movie.Runtime > 0, "runtime", "must be a positive integer")
-
-	v.Check(movie.Genres != nil, "genres", "must be provided")
-	v.Check(len(movie.Genres) >= 1, "genres", "must contain at least 1 genre")
-	v.Check(len(movie.Genres) <= 5, "genres", "must not contain more than 5 genres")
-	v.Check(validator.Unique(movie.Genres), "genres", "must not contain duplicate values")
-}
-
 // Insert method accepts a pointer to a movie struct and insert a new record into the db.
 func (m MovieModel) Insert(movie *Movie) error {
 	query := `
@@ -219,4 +200,23 @@ func (m MovieModel) GetAll(title string, genres []string, filters Filters) ([]*M
 	metadata := calculateMetadata(totalRecords, filters.Page, filters.PageSize)
 
 	return movies, metadata, nil
+}
+
+func ValidateMovie(v *validator.Validator, movie *Movie) {
+	// Use the Check() method to execute our validation checks.
+	// It will add the key and error message to the errors map if the checks are not true.
+	v.Check(movie.Title != "", "title", "must be provided")
+	v.Check(len(movie.Title) <= 500, "title", "must not be more than 500 bytes long")
+
+	v.Check(movie.Year != 0, "year", "must be provided")
+	v.Check(movie.Year >= 1888, "year", "must be greater than 1888")
+	v.Check(movie.Year <= int32(time.Now().Year()), "year", "must not be in the future")
+
+	v.Check(movie.Runtime != 0, "runtime", "must be provided")
+	v.Check(movie.Runtime > 0, "runtime", "must be a positive integer")
+
+	v.Check(movie.Genres != nil, "genres", "must be provided")
+	v.Check(len(movie.Genres) >= 1, "genres", "must contain at least 1 genre")
+	v.Check(len(movie.Genres) <= 5, "genres", "must not contain more than 5 genres")
+	v.Check(validator.Unique(movie.Genres), "genres", "must not contain duplicate values")
 }
